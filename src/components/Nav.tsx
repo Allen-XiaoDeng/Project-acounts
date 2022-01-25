ab4eaf185cae6416afa0be3e7476bcf4b5c03302
allendeng <guolong613@gmail.com>
2022-01-25 14:27:50 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,44 +6,51 @@ import Icon from './Icon';
 
 const NavWrapper = styled.nav`
   line-height: 2.4rem;
-  box-shadow: 0 0 0.3rem rgba(0,0,0,0.25);
+  box-shadow: 0 0 0.3rem rgba(0, 0, 0, 0.25);
+
   > ul {
     display: flex;
-    >li{
+
+    > li {
       width: 33.333333%;
       text-align: center;
+      >a {
       display: flex;
-      padding: 0.4rem;
       flex-direction: column;
       justify-content: center;
       align-items: center;
+      padding: 0.4rem;
       .icon {
         width: 2.4rem;
         height: 2.4rem;
+      }
       }
     }
   }
 `;
 
-const Nav =()=>{
+const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag" />
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页</Link>
         </li>
         <li>
-          <Icon name="money" />
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页</Link>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="chart"/>
+            统计页</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 export default Nav;
