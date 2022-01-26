c891ebb98d8a17c9ef0e560b50468e97095974fe
allendeng <guolong613@gmail.com>
2022-01-26 15:27:33 +0800

    收集表单数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,19 +5,52 @@ import {CategorySection} from './Money/CategorySection';
 import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 import {TagsSection} from './Money/TagsSection';
+import {useState} from 'react';
+
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`;
+
+type Category = '-' | '+'
 
 function Money() {
+  const [selected, setSelected] = useState({
+    tags: [] as string[],
+    note: '',
+    category: '-' as Category,
+    amount: 0
+  });
 
-  const MyLayout = styled(Layout)`
-    display: flex;
-    flex-direction: column;
-  `
   return (
     <MyLayout>
-      <TagsSection />
-      <NoteSection />
-      <CategorySection />
-      <NumberPadSection />
+      <TagsSection value={selected.tags}
+                   onChange={(tags) => setSelected({
+                     ...selected,
+                     tags: tags
+                   })}/>
+      <NoteSection value={selected.note}
+                   onChange={(note) => {
+                     setSelected({
+                       ...selected,
+                       note: note
+                     });
+                   }}/>
+      <CategorySection value={selected.category}
+                       onChange={(category) => {
+                         setSelected({
+                           ...selected,
+                           category: category
+                         });
+                       }}/>
+      <NumberPadSection value={selected.amount}
+                        onChange={(amount) => {
+                          setSelected({
+                            ...selected,
+                            amount: amount
+                          });
+                        }}
+                         onOk={() => {}}/>
     </MyLayout>
   );
 }
--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,46 +1,53 @@
 import styled from 'styled-components';
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 
 const Wrapper = styled.section`
-    background: #f5f5f5;
-    padding: 0 1.6rem;
-    font-size: 1.4rem;
-   > label {
-     display: flex;
-     align-items: center;
-     >span { 
-       margin-right: 1.6rem;
-       white-space: nowrap;}
-     >input {
-       display: block;
-       width: 100%;
-       height: 7.2rem;
-       background: none;
-       border: none;
-     }
-  }
-  `
+  background: #f5f5f5;
+  padding: 0 1.6rem;
+  font-size: 1.4rem;
 
-const NoteSection: React.FC =() =>{
-  const [note, setNote] = useState('');
+  > label {
+    display: flex;
+    align-items: center;
+
+    > span {
+      margin-right: 1.6rem;
+      white-space: nowrap;
+    }
+
+    > input {
+      display: block;
+      width: 100%;
+      height: 7.2rem;
+      background: none;
+      border: none;
+    }
+  }
+`;
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+}
+const NoteSection: React.FC<Props> = (props) => {
+  const note = props.value;
   const refInput = useRef<HTMLInputElement>(null);
   const onBlur = () => {
     if (refInput.current !== null) {
-      setNote(refInput.current.value);
+      props.onChange(refInput.current.value);
     }
   };
-  return(
-     <Wrapper>
-       <label>
-         <span>备注</span>
-         <input type="text" placeholder="在这里添加备注"
-                ref={refInput}
-                defaultValue={note}
-                onBlur={onBlur}
-         />
-       </label>
-     </Wrapper>
-   )
-}
+  return (
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注"
+               ref={refInput}
+               defaultValue={note}
+               onBlur={onBlur}
+        />
+      </label>
+    </Wrapper>
+  );
+};
 
 export {NoteSection};
