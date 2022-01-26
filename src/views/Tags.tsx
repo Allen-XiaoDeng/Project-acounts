3122c44783425ed9afeb7b35071834e7d6e0a5de
allendeng <guolong613@gmail.com>
2022-01-26 16:50:02 +0800

    tags list 样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,15 +1,59 @@
 import Layout from '../components/Layout';
 import * as React from 'react';
 import {useTags} from './Tags/useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
+const TagList = styled.ol`
+  font-size: 1.6rem;
 
+  > li {
+    //#e5e5e7
+    border-bottom: 0.1rem solid #d5d5d9;
+    line-height: 2rem;
+    padding: 1.2rem 1.6rem 1.2rem 0;
+    margin-left: 1.6rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 1.8rem;
+  border: none;
+  padding: 0.8rem 1.2rem;
+  background: #f60;
+  border-radius: 0.4rem;
+  color: white;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Space = styled.div` 
+  height: 1.6rem;
+`
 function Tags() {
-  const {tags,setTags} = useTags();
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      {tags.map(tag =>
-          <li key={tag}>{tag}</li>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right"/>
+          </li>
         )}
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
